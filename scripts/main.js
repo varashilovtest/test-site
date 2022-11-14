51b873efc92183b77d2a2a19f1ea155989e7c045
Алексей Мушкетов <varashilov1995@mail.ru>
2022-11-14 16:06:39 +0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -15,14 +15,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Введите имя раба');
   localStorage.setItem('name', myName);
-  myHeading.textContent = 'Добро пожаловать мой раб ' + myName;
+  myHeading.textContent = 'Добро пожаловать, мой раб ' + myName;
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  myHeading.textContent = 'Добро пожаловать, мой раб ' + storedName;
 }
 
 myButton.onclick = function() {
